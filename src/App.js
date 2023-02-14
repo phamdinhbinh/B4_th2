b7c60884067027b2857248b5baadcc9b8fb6964a
phamdinhbinh <phamdinhbinhonly1@gmail.com>
2023-02-14 16:36:41 +0700

    th2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Box from './Box';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDisplay: true
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  onRemoveBox = () => {
+    this.setState({ isDisplay: false });
+  };
+
+  render() {
+    return (
+      <div className='container'>
+        {this.state.isDisplay && <Box />}
+        <div onClick={this.onRemoveBox}>Xoá component Box</div>
+      </div>
+    );
+  }
 }
 
 export default App;
